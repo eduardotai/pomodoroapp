7b4f54e842751e4e499cf1f11d3cf98d898c91bf
Eduardo Tai De Salles Rodrigues <taisalles@Eduardos-MacBook-Air.local>
2023-09-02 13:32:16 -0300

    feat: add initial setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { GlobalStyle } from "./styles/themes/global";
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/default";
+
+
+
+export function App() {
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello World</h1>
+      <GlobalStyle />
+  </ThemeProvider>
+  )
+}
